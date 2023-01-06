33adc00a49846b02cb4e243103a19806260a26ba
KellyChikanga <kellytchikanga@gmail.com>
2023-01-06 23:32:09 +0000

    upload

---
+++ EvenOdd.js
@@ -0,0 +1,10 @@
+let evenArray = []
+let oddArray = []
+
+function EvenAndOdd(numsArray){
+    for (let i = 0; i < numsArray.length; i++) {
+        if (numsArray[i] % 2 === 0) {
+            evenArray.push(numsArray[i]);
+        } else oddArray.push(numsArray[i]);
+    } return sorted = [evenArray, oddArray]
+}
